6b5ed81103f5c1f7183754439f8fae00dc957b2f
Ard2238 <abhishekrdas28@gmail.com>
2020-10-29 22:41:12 +0530

    Welcome to Employee Payroll Service Database Program

---
+++ src/main/java/EmployeePayrollService.java
@@ -0,0 +1,5 @@
+public class EmployeePayrollService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to The Employee Payroll Service Database Program");
+    }
+}
